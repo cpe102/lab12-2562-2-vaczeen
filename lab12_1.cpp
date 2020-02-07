227c23fbedb33b923bbdbb2d60b2f5b0f99795c7
lab302-31 <lab302-31@labeng.local>
2020-02-07 17:54:37 +0700

    lab12

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -14,7 +14,36 @@ int main()
     cout << "\nS.D. = " << B[1];
     cout << "\nMax = " << B[2];
     cout << "\nMin = " << B[3];
+   
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+	double x=0;
+	double y=0;
+	double sum=0;
+	B[3]=A[0];
+	B[2]=A[1];
+	for(int i=0;i<N;i++){
+		x=x+A[i];
+		B[0]=x/N;
+	}
+	for(int j=0;j<N;j++){
+		y=y+pow(A[j],2);
+		sum=pow((x/N),2);
+		B[1]=sqrt((1.0/N*y)-sum);
+    }
+	for(int k=0;k<N;k++){
+		
+		if(A[k]>B[2]){
+			B[2]=A[k];
+		}
+	}
+	for(int l=0;l<N;l++){
+		if(A[l]<B[3]){
+			B[3]=A[l];
+		}
+	}
 
+
+}
 //Write definition of stat() here 
